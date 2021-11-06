fcc0bce5279acf8ae30c87971ab07201601e2ec4
= <=>
2021-11-06 15:40:00 -0600

    added content to contact and FAQ

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="contact">
-    <h1>This is Contact</h1>
+    <p id="contact-title">
+      We would love to hear from you! Please email us at info@mysite.com or fill
+      in the form and we’ll get back to you within 24 hours.
+    </p>
+    <form>
+      <label for="fname">First name:</label><br />
+      <input type="text" id="fname" name="fname" size="50" /><br />
+      <label for="lname">Last name:</label><br />
+      <input type="text" id="lname" name="lname" size="50" /><br />
+      <label for="email">Email:</label><br />
+      <input type="text" id="email" name="email" size="50" /><br />
+      <label for="msg">Type Message Here:</label><br />
+      <input type="text" id="msg" name="msg" size="50" /><br />
+    </form>
+    <div class="medium-whitespace"></div>
+    <button id="contact-submit">Submit</button>
+    <div class="medium-whitespace"></div>
   </div>
 </template>
 
 <style>
-
+.medium-whitespace {
+  margin: 20px;
+}
+label {
+  font-size: 1.2em;
+}
 </style>
--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,9 +1,43 @@
 <template>
   <div class="faq">
-    <h1>This is FAQ</h1>
+    <div class="centered-headers">
+      <h2>Frequently Asked Questions<br />-</h2>
+      <h4>How long will my highlights last?</h4>
+      <h5>
+        Even though our highlights are usually done with permanent color,
+        eventually they will grow out within 5-8 weeks. We recommend having your
+        highlights touched up every 6 weeks.
+      </h5>
+      <div class="large-whitespace"></div>
+      <h4>What if I want to donate my hair?</h4>
+      <h5>
+        We do this all the time! If you would like to donate your hair to Locks
+        of Love, make sure it is long enough to cut a 10 inch ponytail off. The
+        ponytail needs to be placed in a Ziploc bag and sent in to Locks of
+        Love. Please note that we do not send the hair in here at the salon. It
+        is up to you to send the hair once it is cut.
+      </h5>
+      <div class="large-whitespace"></div>
+      <h4>How do I book my appointment on-line?</h4>
+      <h5>
+        My availability can be found on
+        <a href="/schedule">my calendar.</a> Select any opening that
+        is available and works for you. If you cannot find a time that suits
+        you, please reach out via phone at 555-555-5555 and we can arrange a
+        different time for you appointment.
+      </h5>
+      <div class="large-whitespace"></div>
+    </div>
   </div>
 </template>
 
 <style>
-
+.centered-headers h1, .centered-headers h2, .centered-headers h3, .centered-headers h4,
+.centered-headers h5, .centered-headers h6 {
+  text-align: center;
+  margin: 10px 50px;
+}
+.large-whitespace {
+  margin: 50px;
+}
 </style>
