7ccd3b9f38d002f2f2da7616fc2d0df1ebbbe443
= <=>
2021-11-06 14:42:23 -0600

    shop schedule buttons added which pass the selected item to the calendar to use and select correct appointment duration.

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,20 +1,51 @@
 <template>
   <div class="shop">
     <div class="products-with-image">
-      <div class="product-with-image" v-for="product in $root.$data.products_with_images" :key="product.name">
-        <div class='one-col'>
-          <p class="shop-item title">{{product.name}}&emsp;{{product.price}}</p>
-          <a class="shop-item link" href="/schedule" v-on:click="set_selected_product(product)">Schedule Appointment</a>
+      <div
+        class="product-with-image"
+        v-for="product in $root.$data.products_with_images"
+        :key="product.name"
+      >
+        <div class="one-col">
+          <p class="shop-item title">
+            {{ product.name }}&emsp;{{ product.price }}
+          </p>
+          <button
+            class="shop-item link"
+            v-on:click="set_selected_product(product)"
+          >
+            Schedule Appointment
+          </button>
           <div class="image_title_set">
-            <img v-bind:src="product.image_before"/>
-            <p class='image_description'>Before</p>
+            <img v-bind:src="product.image_before" alt="" />
+            <p class="image_description">Before</p>
           </div>
           <div class="image_title_set">
-            <img v-bind:src="product.image_after"/>
-            <p class='image_description'>After</p>
+            <img v-bind:src="product.image_after" />
+            <p class="image_description">After</p>
           </div>
         </div>
-        <hr class="shop-divider">
+        <hr class="shop-divider" />
+      </div>
+    </div>
+    <p class="shop-item title">Other</p>
+    <div class="products-no-image">
+      <div
+        class="product-no-image"
+        v-for="product in $root.$data.products_no_images"
+        :key="product.name"
+      >
+        <div class="one-col">
+          <p class="shop-item other title">
+            {{ product.name }}&emsp;{{ product.price }}
+          </p>
+          <button
+            class="shop-item link"
+            v-on:click="set_selected_product(product)"
+          >
+            Schedule Appointment
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -22,14 +53,15 @@
 
 <script>
 export default {
-  name: 'Shop',
+  name: "Shop",
   methods: {
     set_selected_product(product) {
       console.log(product);
       this.$root.$data.selected_product = product;
-    }
-  }
-}
+      this.$router.push("/schedule");
+    },
+  },
+};
 </script>
 
 <style>
@@ -38,6 +70,15 @@ export default {
   font-size: 1.5em;
   margin-left: 10px;
   margin-top: 30px;
+}
+
+.link {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.other {
+  margin-top: 10px;
 }
 
 .shop-divider {
